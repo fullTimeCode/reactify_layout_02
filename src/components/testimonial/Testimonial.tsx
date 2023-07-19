281aed62c0eccec4f3a68f82213029f306f42500
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-07-19 09:14:06 +0200

    feat: add starter ts and style files for testimonial component

---
+++ src/components/testimonial/Testimonial.tsx
@@ -0,0 +1,11 @@
+import './testimonial.scss'
+
+const Testimonial = () => { 
+    return (
+        <div className='testimonial'>
+          <h2>Testimonial</h2>
+        </div>
+    )
+}
+
+export default Testimonial
