9f5263a942f8396d2927909a514dcd986d340df1
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-07-19 09:53:31 +0200

    feat: refactor code and locate into the component testimonial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import './App.css'
+import Testimonial from './components/testimonial/Testimonial';
 import testimonialData from "./data.json";
 
 function App() {
-  
+
   return (
     <main className="testimonial-grid">
       {
-        testimonialData.map((testimony, idx) => <article key={idx} className={`testimonial flow ${testimony.colors}`}>
-        <div className="flex">
-          <div>
-            <img  src={testimony.image} alt={`photo of ${testimony.name}`} />
-          </div>
-          <div>
-            <h2 className="name">{ testimony.name }</h2>
-            <p className="position">{ testimony.position }</p>
-          </div>
-          </div>
-          {
-            testimony.testimonial.map ((para, idx) => <p key={idx}>{para}</p>)
-          } 
-        </article>)
-    }
+        testimonialData.map((testimony, idx) =>
+          <Testimonial key={idx} {...testimony} />)
+      }
     </main>
   )
 }
--- src/components/testimonial/Testimonial.tsx
+++ src/components/testimonial/Testimonial.tsx
@@ -1,10 +1,29 @@
-import './testimonial.scss'
+import './testimonial.css'
 
-const Testimonial = () => { 
+type Testimony = {
+  colors: string;
+  name: string;
+  testimonial: string[];
+  position: string;
+  image: string;
+}
+
+const Testimonial = (testimony: Testimony) => {  
     return (
-        <div className='testimonial'>
-          <h2>Testimonial</h2>
-        </div>
+        <article className={`testimonial flow ${testimony.colors}`}>
+          <div className="flex">
+            <div>
+              <img  src={testimony.image} alt={`photo of ${testimony.name}`} />
+            </div>
+            <div>
+              <h2 className="name">{ testimony.name }</h2>
+              <p className="position">{ testimony.position }</p>
+            </div>
+            </div>
+            {
+              testimony.testimonial.map ((para, idx) => <p key={idx}>{para}</p>)
+            } 
+      </article>
     )
 }
 
